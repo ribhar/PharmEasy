5dd5ca60c5834b9f591dad29155c5e4772638fec
Ritik Bhardwaj <varribhar@gmail.com>
2022-02-26 13:41:41 +0530

    udated the main

--- script/index.js
+++ script/index.js
@@ -45,8 +45,11 @@ var slide = document.querySelector(".slide");
     function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.querySelector(".navbar").style.height = "80px";
+        document.querySelector(".overflowbar").style.visibility = "hidden"
+        // console.log(document.querySelector(".overflowbar").innerHTML)
     } else {
         document.querySelector(".navbar").style.height = "123px";
+        document.querySelector(".overflowbar").style.visibility = "visible"
     }
     }
 
